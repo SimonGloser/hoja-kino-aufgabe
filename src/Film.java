66d238261cf66222225482a723c784a2645c4da7
SimonGloser <simon.gloser@googlemail.com>
2019-04-27 12:48:17 +0200

    Initial commit and add class Film and add main to class Kino

---
+++ src/Film.java
@@ -0,0 +1,15 @@
+public class Film {
+    public int laenge;
+    public String autor;
+    public String regiseur;
+    public String titel;
+
+    public Film(int laenge, String autor, String regiseur, String titel){
+        this.laenge = laenge;
+        this.autor = autor;
+        this.regiseur = regiseur;
+        this.titel = titel;
+    }
+
+
+}
